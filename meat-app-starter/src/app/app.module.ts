01602be86bb48adeb6f305719f0a6424bd8c0a87
ernaneaugusto <ernaneatcipriano@gmail.com>
2019-06-19 20:00:49 -0300

    meat-app-starter criado component about e navegacao por rotas menu header

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -7,7 +7,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component'
+
+import { ROUTES } from './app.routes'
 
 
 @NgModule({
@@ -15,12 +18,13 @@ import { HomeComponent } from './home/home.component'
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
