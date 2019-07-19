3b044b2128869a9bc9f37200ca7a6352e97c0ed4
E. de Toledo Cipriano <e.de.toledo.cipriano@avanade.com>
2019-07-18 23:14:00 -0300

    meat-app-starter add comp. reviews e navegacao entre abas pag de restaurant-details

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -17,7 +17,8 @@ import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component'
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 
 
 @NgModule({
@@ -32,7 +33,8 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
     RestaurantDetailComponent,
     MenuComponent,
     ShoppingCartComponent,
-    MenuItemComponent
+    MenuItemComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
--- meat-app-starter/src/app/app.routes.ts
+++ meat-app-starter/src/app/app.routes.ts
@@ -3,10 +3,20 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'restaurants', component: RestaurantsComponent },
-  { path: 'restaurants/:id', component: RestaurantDetailComponent }
+  { path: 'restaurants/:id', component: RestaurantDetailComponent, children: 
+    [
+      // pathMatch: 'full' // o 'full' nesse caso sera utilizado para quando acessar
+      // exatamente a rota /restaurants/:id/menu
+      { path: "", redirectTo: "menu", pathMatch: "full" },
+      { path: "menu", component: MenuComponent },
+      { path: "reviews", component: ReviewsComponent }
+    ] 
+  }
 ]
