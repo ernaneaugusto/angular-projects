394e1b02155302604728515e5e715fdb957ee9fa
ernaneaugusto <ernaneatcipriano@gmail.com>
2019-07-18 21:00:21 -0300

    meat-app-starter restaurants retornando via api com json-server

--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -1,45 +1,16 @@
+import { MEAT_API } from './../app.api';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
 import { Restaurant } from "./restaurant/restaurant.model";
+import "rxjs/add/operator/map";
 
-export class RestaurantsService{
+@Injectable()
+export class RestaurantsService {
+  constructor(private http: Http) { }
 
-  rests: Restaurant[] = [
-    {
-      id: "bread-bakery",
-      name: "Bread & Bakery",
-      category: "Bakery",
-      deliveryEstimate: "25m",
-      rating: 4.9,
-      imagePath: "assets/img/restaurants/breadbakery.png"
-    },
-    {
-      id: "burger-house",
-      name: "Burger House",
-      category: "Hamburgers",
-      deliveryEstimate: "100m",
-      rating: 3.5,
-      imagePath: "assets/img/restaurants/burgerhouse.png"
-    },
-    {
-      id: "coffee-corner",
-      name: "Coffee Corner",
-      category: "Coffee Shop",
-      deliveryEstimate: "30-40m",
-      rating: 4.8,
-      imagePath: "assets/img/restaurants/coffeecorner.png"
-    },
-    {
-      id: "green-food",
-      name: "Green Food",
-      category: "Saudável",
-      deliveryEstimate: "75m",
-      rating: 4.1,
-      imagePath: "assets/img/restaurants/greenfood.png"
-    }
-  ]
-
-  constructor(){}
-
-  restaurants(): Restaurant[] {
-    return this.rests;
+  restaurants(): Observable<Restaurant[]> {
+    return this.http.get(`${MEAT_API}/restaurants`)
+            .map(response => response.json());
   }
 }
