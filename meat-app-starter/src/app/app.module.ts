b99ee757bf7e4907265c2ac3845774e5abf4a67d
E. de Toledo Cipriano <e.de.toledo.cipriano@avanade.com>
2019-07-22 20:18:18 -0300

    meat-app-starter add service shopping-cart e model CartItem

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 
 
 @NgModule({
@@ -41,7 +42,10 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService],
+  providers: [
+    RestaurantsService,
+    ShoppingCartService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ meat-app-starter/src/app/restaurant-detail/shopping-cart/cart-item.model.ts
@@ -0,0 +1,11 @@
+import { MenuItem } from "../menu-item/menu-item.model";
+
+export class CartItem {
+
+    value(): number{
+        let total = this.menuItem.price * this.quantity;
+        return total;
+    }
+
+    constructor(public menuItem: MenuItem, public quantity: number = 0) { }
+}
