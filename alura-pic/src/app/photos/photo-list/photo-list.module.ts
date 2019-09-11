2e0be5c24bd3e06c016473549acecf8e9581c453
E. de Toledo Cipriano <e.de.toledo.cipriano@avanade.com>
2019-09-11 14:44:42 -0300

    Componentes em modulos

---
+++ alura-pic/src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
+
+import { PhotoListComponent } from './photo-list.component';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { PhotosComponent } from './photos/photos.component';
+import { FilterByDescriptionPipe } from './filter-by-description.pipe';
+import { PhotoModule } from '../photo/photo.module';
+
+@NgModule({
+    declarations: [
+        PhotoListComponent,
+        PhotosComponent,
+        LoadButtonComponent,
+        FilterByDescriptionPipe
+    ],
+    imports: [
+        CommonModule,
+        PhotoModule
+    ]
+})
+
+export class PhotoListModule { }
--- alura-pic/src/app/photos/photos.module.ts
+++ alura-pic/src/app/photos/photos.module.ts
@@ -1,26 +1,13 @@
 import { NgModule } from "@angular/core";
-import { HttpClientModule } from '@angular/common/http';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { CommonModule } from '@angular/common';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
 
 @NgModule({
-    declarations: [
-        PhotoComponent,
-        PhotoListComponent,
-        PhotoFormComponent,
-        PhotosComponent,
-        FilterByDescriptionPipe,
-        LoadButtonComponent
-    ],
+    declarations: [],
     imports: [
-        CommonModule,
-        HttpClientModule
+        PhotoFormModule,
+        PhotoListModule
     ]
 })
 
