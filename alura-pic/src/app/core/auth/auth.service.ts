d7720396452ea72095ccbe20f4b1568d746ba968
E. de Toledo Cipriano <e.de.toledo.cipriano@avanade.com>
2019-09-12 16:13:16 -0300

    Inserido pipe na autenticacao para pegar TOKEN

--- alura-pic/src/app/core/auth/auth.service.ts
+++ alura-pic/src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000';
 
@@ -10,7 +11,16 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  authenticate(userName: string, password: string) { 
-    return this.http.post(`${API_URL}/user/login`, { userName, password })
+  authenticate(userName: string, password: string) {
+    return this.http
+      .post(
+        `${API_URL}/user/login`,
+        { userName, password },
+        { observe: 'response' }
+      )
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        console.log(`Usuário ${userName} autenticado com TOKEN: ${authToken}`);
+      }))
   }
 }
