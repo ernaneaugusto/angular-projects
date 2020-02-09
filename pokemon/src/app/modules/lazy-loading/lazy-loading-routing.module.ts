69f2660480a0c44a009209182c572064da15b49a
ernaneaugusto <ernaneatcipriano@gmail.com>
2020-02-09 00:06:26 -0300

    add about component

--- pokemon/src/app/modules/lazy-loading/lazy-loading-routing.module.ts
+++ pokemon/src/app/modules/lazy-loading/lazy-loading-routing.module.ts
@@ -1,10 +1,12 @@
 import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from '@angular/core';
 import { HomeComponent } from 'src/app/components/home/home.component';
+import { AboutComponent } from 'src/app/components/about/about.component';
 
 
 const ROUTES: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'sobre', component: AboutComponent }
 ];
 
 @NgModule({
--- pokemon/src/app/modules/lazy-loading/lazy-loading.module.ts
+++ pokemon/src/app/modules/lazy-loading/lazy-loading.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from "@angular/common";
 
 import { LazyLoadingRoutingModule } from "./lazy-loading-routing.module";
 import { HomeComponent } from 'src/app/components/home/home.component';
+import { AboutComponent } from 'src/app/components/about/about.component';
 
 @NgModule({
     declarations: [
-        HomeComponent
+        HomeComponent,
+        AboutComponent
     ],
     imports: [
         CommonModule,
