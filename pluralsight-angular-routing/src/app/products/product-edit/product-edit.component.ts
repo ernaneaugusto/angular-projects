ad230f3fcc13edeaa670e672b6ac1deca4f56aa7
ernaneaugusto <ernaneatcipriano@gmail.com>
2020-02-15 21:57:04 -0300

    criado pag de edicao e cricao de produtos

--- pluralsight-angular-routing/src/app/products/product-edit/product-edit.component.ts
+++ pluralsight-angular-routing/src/app/products/product-edit/product-edit.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { MessageService } from '../../messages/message.service';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.css']
 })
-export class ProductEditComponent {
+export class ProductEditComponent implements OnInit {
   pageTitle = 'Product Edit';
   errorMessage: string;
 
   product: Product;
 
-  constructor(private productService: ProductService,
-              private messageService: MessageService) { }
+  constructor(
+    private productService: ProductService,
+    private messageService: MessageService,
+    private activatedRoute: ActivatedRoute,
+    private route: Router
+  ) { }
+
+  ngOnInit() {
+    // Com this.route.snapshot.paramMap sem o susbcribe o componente eh atualizado apenas 1 vez, com isso, caso algum parametro da rota seja mudado, como o ID do produto, o componente nao eh atualizado com as novas informacoes desse produto, para que essa atualizacao ocorra deve-se utilizar o subscribe
+    
+    // const id = parseInt(this.route.snapshot.paramMap.get('id'));
+    // this.getProduct(id);
+    this.activatedRoute.paramMap.subscribe(
+      params => {
+        const id = parseInt(params.get('id'));
+        this.getProduct(id);
+      }
+    );
+  }
 
   getProduct(id: number): void {
     this.productService.getProduct(id).subscribe({
@@ -77,5 +95,6 @@ export class ProductEditComponent {
     }
 
     // Navigate back to the product list
+    this.route.navigateByUrl('/products');
   }
 }
