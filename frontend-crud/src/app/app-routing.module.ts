505ac8251953d67c60ea78ff36f913d1eb0b45a8
ernaneaugusto <ernaneatcipriano@gmail.com>
2020-04-18 15:49:44 -0300

    add product-list

--- frontend-crud/src/app/app-routing.module.ts
+++ frontend-crud/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
 
-const routes: Routes = [];
+const ROUTES: Routes = [ 
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductCrudComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
