e4e866664250d74c554487af019fda60743177d1
ernaneaugusto <ernaneatcipriano@gmail.com>
2020-04-18 15:52:07 -0300

    add component product-create

--- frontend-crud/src/app/app-routing.module.ts
+++ frontend-crud/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/products/product-create/product-create.component';
 
 
 const ROUTES: Routes = [ 
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductCrudComponent },
+  { path: 'products/create', component: ProductCreateComponent },
 ];
 
 @NgModule({
