3009538d80f5ba08049d8f66488390f7858cdb0a
ernaneaugusto <ernaneatcipriano@gmail.com>
2020-04-19 02:05:07 -0300

    refatorado service para tratar erros

--- frontend-crud/src/app/services/products.service.ts
+++ frontend-crud/src/app/services/products.service.ts
@@ -1,9 +1,10 @@
+import { map, catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
 import { ProductModel } from './../components/products/product.model';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { Observable } from 'rxjs';
+import { Observable, EMPTY } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
@@ -17,31 +18,62 @@ export class ProductsService {
 		private snackBar: MatSnackBar
 	) { }
 
-	public showMessage(msg: string): void {
+	public showMessage(msg: string, isError = false): void {
 		this.snackBar.open(msg, 'x', {
 			duration: 2000,
 			horizontalPosition: 'right',
-			verticalPosition: 'top'
+			verticalPosition: 'top',
+			panelClass: isError ? ['alert-danger'] : ['alert-success']
 		})
 	}
 
 	public create(product: ProductModel): Observable<ProductModel> {
-		return this.http.post<ProductModel>(`${this.baseUrl}/products`, product);
+		return this.http.post<ProductModel>(`${this.baseUrl}/products`, product)
+			.pipe(
+				map(error => error),
+				catchError(e => this.handleError(e))
+			);
 	}
 
 	public getProducts(): Observable<ProductModel[]> {
-		return this.http.get<ProductModel[]>(`${this.baseUrl}/products`);
+		return this.http.get<ProductModel[]>(`${this.baseUrl}/products`)
+			.pipe(
+				map(error => error),
+				catchError(e => this.handleError(e))
+			);
 	}
-	
+
 	public getProductsById(id: string): Observable<ProductModel> {
-		return this.http.get<ProductModel>(`${this.baseUrl}/products/${id}`);
+		return this.http.get<ProductModel>(`${this.baseUrl}/products/${id}`)
+			.pipe(
+				map(error => error),
+				catchError(e => this.handleError(e))
+			);
 	}
-	
+
 	public update(product: ProductModel): Observable<ProductModel> {
-		return this.http.put<ProductModel>(`${this.baseUrl}/products/${product.id}`, product);
+		return this.http.put<ProductModel>(`${this.baseUrl}/products/${product.id}`, product)
+			.pipe(
+				map(error => error),
+				catchError(e => this.handleError(e))
+			);
 	}
-	
+
 	public delete(id: string): Observable<ProductModel> {
-		return this.http.delete<ProductModel>(`${this.baseUrl}/products/${id}`);
+		return this.http.delete<ProductModel>(`${this.baseUrl}/products/${id}`)
+			.pipe(
+				map(error => error),
+				catchError(e => this.handleError(e))
+			);
+	}
+
+	private handleError(error): Observable<any> {
+		// verifica se json-server esta sendo executado
+		if (error.statusText == 'Unknown Error' && error.status == 0) {
+			alert('JSON Server não está sendo executado! \nVerifique mais informações em README.md');
+		}
+
+		this.showMessage(`Erro ao realizar operação!`, true);
+		return EMPTY;
 	}
 }
