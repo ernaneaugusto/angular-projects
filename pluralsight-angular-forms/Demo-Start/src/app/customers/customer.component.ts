58f881b1f090fecbfe775476c63612c7daae4707
ernaneaugusto <ernaneatcipriano@gmail.com>
2020-05-26 23:42:01 -0300

    add validacao com FormBuilder

--- pluralsight-angular-forms/Demo-Start/src/app/customers/customer.component.ts
+++ pluralsight-angular-forms/Demo-Start/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -27,15 +27,64 @@ export class CustomerComponent implements OnInit {
    * Ex: public nomeDaPropriedade = new FromControl()
    * 
    */
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true),
-    })
+    // Modelo para criacao de reactive forms com FormGroup
+    // this.customerForm = new FormGroup({
+    //   firstName: new FormControl(),
+    //   lastName: new FormControl(),
+    //   email: new FormControl(),
+    //   sendCatalog: new FormControl(true),
+    // });
+
+
+    /**
+     * Modelo de configuracoes para o FormBuilder(as chaves do objeto sao os identificadores utilizados na propriedade formControlName):
+     * 
+     * >>> Exemplo 1:
+     *  this.customerFrom = this.fb.group({
+     *    firstName: '',
+     *    sendCatalog: true
+     * })
+     * 
+     * >>> Exemplo 2:
+     *  this.customerFrom = this.fb.group({
+     *    firstName: '',
+     *    email: false
+     * });
+     * 
+     * >>> Exemplo 2:
+     *  this.customerFrom = this.fb.group({
+     *    firstName: { value: 'Joao', disabled: true },
+     *    email: { value: '', disabled: false }
+     * });
+     * 
+     * >>> Exemplo 3:
+     *  this.customerFrom = this.fb.group({
+     *    firstName: [''],
+     *    email: [{ value: '', disabled: false }]
+     * })
+     */
+
+    this.customerForm = this.fb.group({
+      firstName: [
+        '',
+        [Validators.required, Validators.minLength(3)]
+      ],
+      lastName: [
+        '',
+        [Validators.required, Validators.maxLength(50)]
+      ],
+      email: [
+        '',
+        [Validators.required]
+      ],
+      sendCatalog: [
+        true,
+        []
+      ]
+    });
   }
 
   public save() {
@@ -45,18 +94,18 @@ export class CustomerComponent implements OnInit {
 
   public populateTestData() {
     // setValue: utilizado para alterar todos os campos do formulario
-    // this.customerForm.setValue({
-    //   firstName: 'FirstName setValue',
-    //   lastName: 'Lastname setValue',
-    //   email: 'E-mail setValue',
-    //   sendCatalog: false
-    // });
+    this.customerForm.setValue({
+      firstName: 'FirstName setValue',
+      lastName: 'Lastname setValue',
+      email: 'E-mail setValue',
+      sendCatalog: false
+    });
 
     // patchValue: utilizado para alterar apenas alguns campos do formulario    
-    this.customerForm.patchValue({
-      firstName: 'FirstName setValue',
-      lastName: 'Lastname setValue'
-    });
+    // this.customerForm.patchValue({
+    //   firstName: 'FirstName patchValue',
+    //   lastName: 'Lastname patchValue'
+    // });
 
     this.testData = true;
   }
