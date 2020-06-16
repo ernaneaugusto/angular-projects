58fa7c6cd8494c29dce5c3bc93f6d43cbfa5e79c
ernaneaugusto <ernaneatcipriano@gmail.com>
2020-06-15 22:54:41 -0300

    criado branch curso-angular-caelum e config iniciais

--- README.md
+++
@@ -1,2 +0,0 @@
-# Importante
-Os projetos estão dividos em branchs, ao baixar o projeto execute o comando `git checkout nomeDaBranch` para alterar para uma branch específica.
---
+++ curso-angular-caelum/src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'cmail-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent {
+    private _isMenuOpen = false; // configura abertura e fechamento menu mobile
+
+    get isMenuOpen() {
+        return this._isMenuOpen;
+    }
+
+    public toggleMenu() {
+        this._isMenuOpen = !this._isMenuOpen;
+    }
+}
