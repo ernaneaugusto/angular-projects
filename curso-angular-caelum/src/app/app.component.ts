7958c93d33a8520f7be361fc8cf788bb3d675557
ernaneaugusto <ernaneatcipriano@gmail.com>
2020-06-16 21:53:38 -0300

    criado formulario envio de email

--- curso-angular-caelum/src/app/app.component.ts
+++ curso-angular-caelum/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso-angular-caelum';
+	private _isNewEmailFormOpen = false;
+	public emailList = [];
+	public email = {
+		destinatario: '',
+		assunto: '',
+		conteudo: ''
+	}
+
+	get isNewEmailFormOpen() {
+		return this._isNewEmailFormOpen;
+	}
+
+	public toggleNewEmailForm() {
+		this._isNewEmailFormOpen = !this.isNewEmailFormOpen;
+	}
+
+	public handleNewEmail(event: Event) {
+		event.preventDefault();
+
+		this.emailList.push(this.email);
+
+		this.email = {
+			destinatario: '',
+			assunto: '',
+			conteudo: ''
+		}
+	}
+
 }
