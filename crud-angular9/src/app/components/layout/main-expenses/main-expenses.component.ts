20933d69951c7027e1f7e21f9c46c691bf8ff553
ernaneaugusto <ernaneatcipriano@gmail.com>
2020-06-21 00:02:47 -0300

    validacao form expenses

--- crud-angular9/src/app/components/layout/main-expenses/main-expenses.component.ts
+++ crud-angular9/src/app/components/layout/main-expenses/main-expenses.component.ts
@@ -1,15 +1,46 @@
+import { Utils } from './../../../shared/utils';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main-expenses',
-  templateUrl: './main-expenses.component.html',
-  styleUrls: ['./main-expenses.component.scss']
+	selector: 'app-main-expenses',
+	templateUrl: './main-expenses.component.html',
+	styleUrls: ['./main-expenses.component.scss']
 })
 export class MainExpensesComponent implements OnInit {
 
-  constructor() { }
+	public formExpenses: FormGroup = new FormGroup({
+		amount: new FormControl(
+			'',
+			[Validators.required]
+		),
+		description: new FormControl(
+			'',
+			[Validators.required, Validators.minLength(2)]
+		),
+		purchaseDate: new FormControl(
+			'',
+			[Validators.required]
+		),
+		categorie: new FormControl(
+			'',
+			[Validators.required]
+		),
+		tag: new FormControl(
+			''
+		)
+	})
 
-  ngOnInit(): void {
-  }
+	constructor() { }
 
+	ngOnInit(): void { }
+
+	public submitFormExpenses() {
+		if (this.formExpenses.valid) {
+			console.log("## Valido", this.formExpenses.controls);
+		} else {
+			Utils.markFormFieldsAsTouched(this.formExpenses);
+			console.log("## INVALIDO");
+		}
+	}
 }
